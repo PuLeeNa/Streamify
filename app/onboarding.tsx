6d160b5dfd04cf0489af6bdd68cdce708fa985a4
Your Name <wickramasooriyabgpv.22@uom.lk>
2025-11-20 21:59:37 +0530

    Add onboarding screen and link to it from the index

---
+++ app/onboarding.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+const onboarding = () => {
+  return (
+    <View>
+        <Text>Onboarding</Text>
+    </View>
+  )
+}
+
+export default onboarding
+const styles = StyleSheet.create({})
