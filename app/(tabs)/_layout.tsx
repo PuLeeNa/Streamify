0c88af9342eee534a30e3dd81923d1963baaefd5
Your Name <wickramasooriyabgpv.22@uom.lk>
2025-11-21 02:47:46 +0530

    Add icon and image constants, define movie interfaces, and declare PNG module

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,16 +1,46 @@
-import { Tabs } from 'expo-router'
-import React from 'react'
-import { View, Text } from 'react-native'
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import { Tabs } from "expo-router";
+import React from "react";
+import { View, Text, ImageBackground, Image } from "react-native";
+
+const TabIcon = () => {
+  return (
+    <ImageBackground
+      source={images.highlight}
+      className="flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden"
+    >
+      <Image source={icons.home} tintColor="#151312" className="size-5" />
+      <Text className="text-secondary text-base font-semibold ml-2">Home</Text>
+    </ImageBackground>
+  );
+};
 
 const _Layout = () => {
   return (
     <Tabs>
-        <Tabs.Screen name="index" options={{ headerShown: false, title: 'Home' }} />
-        <Tabs.Screen name="search" options={{ headerShown: false, title: 'Search' }} />
-        <Tabs.Screen name="saved" options={{ headerShown: false, title: 'Saved' }} />
-        <Tabs.Screen name="profile" options={{ headerShown: false, title: 'Profile' }} />
+      <Tabs.Screen
+        name="index"
+        options={{
+          headerShown: false,
+          title: "Home",
+          tabBarIcon: ({ focused }) => <TabIcon />,
+        }}
+      />
+      <Tabs.Screen
+        name="search"
+        options={{ headerShown: false, title: "Search" }}
+      />
+      <Tabs.Screen
+        name="saved"
+        options={{ headerShown: false, title: "Saved" }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{ headerShown: false, title: "Profile" }}
+      />
     </Tabs>
-  )
-}
+  );
+};
 
-export default _Layout
+export default _Layout;
